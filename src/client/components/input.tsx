53fd40213955ded6f74743900652218c3c7da562
Vlad1m1ru5 <kubantsev.vov@mai.ru>
2020-04-26 17:42:47 +0300

    typescript
    input box shadow

--- src/client/components/input.tsx
+++ src/client/components/input.tsx
@@ -5,7 +5,13 @@ const Input = styled.input<{
   type: 'checkbox' | 'text'
 }>`
   border-radius: ${({ type }) => type === 'text' ? '5px' : 'none'};
+  border: none;
+  box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.3);
   height: 24;
+
+  &:hover {
+    box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.3), 0px 0px 6px rgba(0, 0, 0, 0.15);
+  }
 `
 
 export default Input
