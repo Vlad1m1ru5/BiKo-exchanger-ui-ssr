fe84f5deb11e345d541d5f0c972d2b198874acbc
Vlad1m1ru5 <kubantsev.vov@mai.ru>
2020-05-08 21:09:57 +0300

    finder
    modal widget overflow hidden

--- src/client/components/modal.tsx
+++ src/client/components/modal.tsx
@@ -5,6 +5,7 @@ const Modal = styled.div<{ theme: Theme }>`
   box-shadow: ${({ theme }) => theme.box.shadow.bottom};
   position: absolute;
   margin: ${({ theme }) => theme.atom.margin};
+  overflow: hidden;
   top: 0;
   width: 596px;
 `
